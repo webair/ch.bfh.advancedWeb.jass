6a0870689b939db4c783953844a439f4b32c68b3
Stefan Käser <info.stef.kaeser@gmail.com>
2011-12-29 16:14:56 +0100

    Comment enums

--- src/main/java/ch/frickler/jass/db/enums/CardFamily.java
+++ src/main/java/ch/frickler/jass/db/enums/CardFamily.java
@@ -1,5 +1,10 @@
 package ch.frickler.jass.db.enums;
 
+/**
+ * this enum stores card family types, of witch a card consist. 
+ * @author kaeserst
+ *
+ */
 public enum CardFamily {
 	HERZ, SCHAUFEL, KREUZ, ECKEN
 }
--- src/main/java/ch/frickler/jass/db/enums/GameKind.java
+++ src/main/java/ch/frickler/jass/db/enums/GameKind.java
@@ -1,5 +1,10 @@
 package ch.frickler.jass.db.enums;
 
+/**
+ * this enum stores all GameKinds, witch the annoncer can choose.
+ * @author kaeserst
+ *
+ */
 public enum GameKind {
 	TOPDOWN, BOTTOMUP, TRUMPHeart, TRUMPCross, TRUMPBucket,TRUMPEdge
 }
