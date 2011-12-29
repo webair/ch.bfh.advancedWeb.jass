5be588c8bb8647cf3f56cc3b3943e243b9d1bc21
Stefan Käser <info.stef.kaeser@gmail.com>
2011-12-29 16:30:45 +0100

    Comment definitions

--- src/main/java/ch/frickler/jass/definitions/JassAction.java
+++ src/main/java/ch/frickler/jass/definitions/JassAction.java
@@ -3,16 +3,35 @@ package ch.frickler.jass.definitions;
 import ch.frickler.jass.db.entity.User;
 import ch.frickler.jass.service.GameService;
 
+/**
+ * this is the abstract class for the action a user can do during the game
+ * @author kaeserst
+ *
+ */
 public abstract class JassAction {
 		
 		protected User user;
 		
+		/**
+		 * Constructor. we need the users witch want to do the action.
+		 * @param user
+		 */
 		public JassAction(User user){
 			this.user = user;
 		}
 		
-		public abstract boolean doAction(GameService spiel);
+		/**
+		 * 
+		 * @param the game for witch the action is.
+		 * @return
+		 */
+		public abstract boolean doAction(GameService game);
 
+		/**
+		 * 
+		 * @param check if the action  the user wants to proceed is possible or not.
+		 * @return
+		 */
 		public abstract boolean isActionPossible(GameService game);
 
 	}
